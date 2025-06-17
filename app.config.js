fdc6fe6c408e17b737d6fcf91d779dca09c7e4fa
Andreas Reuter <5789390+andreasreuter@users.noreply.github.com>
2025-06-17 22:28:09 +0200

    chore(eslint): Fix ESLint and Prettier configs

---
+++ app.config.js
@@ -0,0 +1,42 @@
+export default ({ config }) => ({
+  expo: {
+    name: "NIPs Chatbot",
+    slug: "io.nbuild.NIPsChatbot",
+    version: "0.1.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "io.nbuild.Chat",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      },
+      edgeToEdgeEnabled: true
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/favicon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/splash-icon.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#ffffff"
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    }
+  }
+});
--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,47 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require("eslint/config")
 const expoConfig = require("eslint-config-expo/flat")
+const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended")
 
 module.exports = defineConfig([
   expoConfig,
+  eslintPluginPrettierRecommended,
   {
-    ignores: ["dist/*"],
+    rules: {
+      semi: ["error", "never"],
+      quotes: ["error", "double"],
+      "prefer-const": "error",
+      indent: ["error", 2],
+      "max-len": [
+        "error",
+        {
+          code: 80,
+          ignoreUrls: true,
+          tabWidth: 2,
+        },
+      ],
+      "no-extra-parens": [
+        "error",
+        "all",
+        {
+          conditionalAssign: false,
+          returnAssign: false,
+          ignoreJSX: "all",
+        },
+      ],
+      "react/no-unknown-property": [
+        2,
+        {
+          ignore: ["jsx"],
+        },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 80,
+          semi: false,
+        },
+      ],
+    },
+    ignores: ["dist/*", "node_modules", ".expo"],
   },
 ])
